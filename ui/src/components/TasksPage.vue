ae7affe62f5f427392a24fd2951fcdb4a22439ec
Juneroy Quinimon <eroyjune@gmail.com>
2023-08-26 18:56:24 +0800

    feat: add link redirect to create

--- ui/src/components/TasksPage.vue
+++ ui/src/components/TasksPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <router-link class="text-start w-100 d-flex" to="/create">New Task</router-link>
     <b-list-group :key="index" v-for="(task, index) in tasks">
       <b-list-group-item>
         <task-page :task="task" />
