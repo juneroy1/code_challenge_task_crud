9c54d393509bb798b0cdd378cb38170468a56b00
Juneroy Quinimon <eroyjune@gmail.com>
2023-08-26 18:56:41 +0800

    feat:enhance UI for list

--- ui/src/components/TaskPage.vue
+++ ui/src/components/TaskPage.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
-    <div class="d-flex flex-column justify-content-start align-items-start">
-      <h3>
-        {{ task.title }}
-      </h3>
-      <p>{{ task.description }}</p>
+    <div class="d-flex flex-row justify-content-start align-items-start">
+      <b-container
+        class="d-flex flex-column justify-content-start align-items-start"
+      >
+        <h3>
+          {{ task.title }}
+        </h3>
+        <p>{{ task.description }}</p>
+      </b-container>
+      <div class="d-flex flex-column justify-content-start align-items-end">
+        <b-container
+          class="d-flex flex-row justify-content-start align-items-end"
+        >
+          <b-button class="mx-2" @click="redirectEditForm(task.id)" variant="primary">Edit</b-button>
+          <b-button variant="danger">Delete</b-button>
+        </b-container>
+      </div>
     </div>
   </div>
 </template>
