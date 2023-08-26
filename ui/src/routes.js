0243806ed4561b76e3c82c25a587a5d277e2b271
Juneroy Quinimon <eroyjune@gmail.com>
2023-08-26 18:57:25 +0800

    feat: implement edit func and form

--- ui/src/routes.js
+++ ui/src/routes.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 // routes.js
-import TaskCreatePage from "./pages/TaskCreatePage.vue";
+import TaskFormPage from "./pages/TaskFormPage.vue";
 import TasksListPage from "./pages/TasksListPage.vue";
 Vue.use(VueRouter);
 const routes = [
   { path: "/", component: TasksListPage },
-  { path: "/create", component: TaskCreatePage },
+  { path: "/create", component: TaskFormPage },
+  { path: "/edit/:id", component: TaskFormPage },
 ];
 
 export default new VueRouter({
--- ui/src/utils/TaskCreateMixin.js
+++ ui/src/utils/TaskCreateMixin.js
@@ -10,6 +10,7 @@ export default () => {
           due_date: null,
           status: false,
         },
+        create: true,
       };
     },
     methods: {
@@ -21,12 +22,29 @@ export default () => {
           .post("http://localhost:8000/api/task", this.task)
           .then((response) => {
             console.log("response", response);
-            this.$router.push('/');
+            this.$router.push("/");
           })
           .catch((error) => {
             console.error("Error creating data:", error);
           });
       },
+      getTaskDetails(id) {
+        axios
+          .get(`http://localhost:8000/api/task/${id}/edit`)
+          .then((response) => {
+            console.log("response edit", response);
+            // this.$router.push('/');
+          })
+          .catch((error) => {
+            console.error("Error getting data:", error);
+          });
+      },
+    },
+    created() {
+      if (this.$route.params.id) {
+        this.getTaskDetails(this.$route.params.id);
+        this.create = false;
+      }
     },
   };
 };
